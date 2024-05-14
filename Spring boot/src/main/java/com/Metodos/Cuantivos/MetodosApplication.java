2440320a89fc11a9d2ec15b973e9ef9f98e3c1c9
FlavioJg <flaviojfranchich95@gmail.com>
2024-05-14 11:06:19 -0400

    Se inicia el proyecto de spring boot

---
+++ Spring boot/src/main/java/com/Metodos/Cuantivos/MetodosApplication.java
@@ -0,0 +1,13 @@
+package com.Metodos.Cuantivos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetodosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetodosApplication.class, args);
+	}
+
+}
