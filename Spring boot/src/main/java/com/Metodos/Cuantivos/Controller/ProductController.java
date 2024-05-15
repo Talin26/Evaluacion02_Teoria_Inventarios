706a6053f3fcc79ea76f4c671143380cac84856e
Samir <abderalvarez@gmail.com>
2024-05-15 11:09:18 -0400

    cors solucionado

--- Spring boot/src/main/java/com/Metodos/Cuantivos/Controller/ProductController.java
+++ Spring boot/src/main/java/com/Metodos/Cuantivos/Controller/ProductController.java
@@ -6,11 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ProductController {
     @Autowired
     private ProductService productService;
